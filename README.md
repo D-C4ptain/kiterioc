7c3df7354638b33fc0eeef59c5e0cff5dadcad84
d-captainkenya <dcaptainkenya@gmail.com>
2023-01-24 10:38:30 +0300

    sysinfo

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # Kiterioc
 
 A multiplatform IOC hunter for Cyber Threat Intelligence. 
+
+
+
+Data measurement units can be classified into the following:
+
+    Bit - The smallest unit of computer data measurement. Has the value of 0 or 1, which correspond to electronic values of on or off.
+    Byte - Contains 8 bits and enough information to form and store at least a single ASCII character, for e.g. 'a'.
+    KiloByte - Contains about 1024 Bytes.
+    MegaBytes - Contains about 1024 KiloBytes.
+    GigaBytes - Contains about 1024 MegaBytes.
+
+There are larger units of data measurement as well. The following units have been used in this project.
+KiloByte (KB) 	1024 Bytes
+MegaByte (MB) 	1024 KiloBytes
+GigaByte (GB) 	1024 MegaBytes
+
+Output is in 'GB', you can easily modify sysinfo.py to any other unit.
--- kiterioc.py
+++ kiterioc.py
@@ -1,1 +1,47 @@
-print("IOC Hunter")
+# ! /usr/bin/python3
+
+from colorama import Fore, Back, Style
+
+def banner():
+    print(Fore.RED + """  
+        ##    ## #### ######## ######## ########  ####  #######   ######
+        ##   ##   ##     ##    ##       ##     ##  ##  ##     ## ##    ## 
+        ##  ##    ##     ##    ##       ##     ##  ##  ##     ## ##       
+        #####     ##     ##    ######   ########   ##  ##     ## ##       
+        ##  ##    ##     ##    ##       ##   ##    ##  ##     ## ##       
+        ##   ##   ##     ##    ##       ##    ##   ##  ##     ## ##    ## 
+        ##    ## ####    ##    ######## ##     ## ####  #######   ###### 
+        From the hills of Kitere - https://d-captainkenya.github.io
+        """)
+    print(Style.RESET_ALL)
+
+def systeminfo():
+    #https://gist.github.com/emrekgn/af9783af041edc3d508acac35dade9d2
+    #https://github.com/darkwizard242/system-info
+        
+    import sysinfo
+    
+
+
+
+
+
+
+
+
+
+
+
+def hunt():
+    return 0
+    
+def help():
+    return 0
+    
+    
+if __name__ == "__main__":
+    banner()
+    systeminfo()
+    hunt()
+    # if args
+    help()
